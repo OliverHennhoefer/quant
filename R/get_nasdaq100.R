5eefd300f091ddf6f27763ee8ad703176142356f
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2022-01-04 21:54:21 +0100

    Adds get_nasdaq100 and get_sp500
    Functions scrape current index composition with respective stock symbols

---
+++ R/get_nasdaq100.R
@@ -0,0 +1,16 @@
+get_nasdaq100 <- function(raw = FALSE) {
+
+  url <- "https://en.wikipedia.org/wiki/Nasdaq-100"
+  xpath <- '//*[@id="constituents"]'
+
+  table <- rvest::read_html(url) %>%
+    rvest::html_nodes(xpath = xpath) %>%
+    rvest::html_table() %>%
+    as.data.frame()
+
+  colnames(table) <- c("Company", "Symbol", "Sector", "Subsector")
+
+  if (raw) return(table)
+  return(table[, 2, drop = FALSE])
+}
+
