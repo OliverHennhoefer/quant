05d39ff30080c0ff83eed35e5def1818559f5559
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2022-01-08 14:10:13 +0100

    Bugfix

--- R/get_insider_ownership.R
+++ R/get_insider_ownership.R
@@ -12,7 +12,7 @@
 get_insider_ownership <- function(df) {
   check_sanity(df)
 
-  df$insider_ownership <- NA
+  dfe <- data.frame()
   rows <- nrow(df)
   pbar <- utils::txtProgressBar(0, rows, style = 3)
 
@@ -21,12 +21,31 @@ get_insider_ownership <- function(df) {
 
     ticker <- df$Symbol[i]
     url <- paste0("https://www.gurufocus.com/term/InsiderOwnership/", ticker, "/")
-    xpath <- '//*[@id="def_body_detail_height"]/font'
-    ttm <- get_html_text(url, xpath = xpath)
 
+    xpath = '//*[@id="target_def_historical_data"]/div[2]/table'
+    table <- get_html_table(url, xpath = xpath, raw = TRUE)[, -1]
+
+    xpath = '//*[@id="def_body_detail_height"]/font'
+    ttm <- get_html_text(url, xpath = xpath)
     extr_str <- gsub("\\s*\\([^\\)]+\\)", "\\1", ttm)
-    df$insider_ownership[i] <- as.numeric(gsub("[^0-9.-]+", "\\1", extr_str))
+    cval <- as.numeric(gsub("[^0-9.-]+", "\\1", extr_str))
+
+    if (any(dim(table) == FALSE)) {
+      ic_table <- data.frame("Symbol" = ticker, "insider_ownership_ttm" = cval)
+    } else {
+      colnames(table) <- gsub("-", "_", paste0("insider_ownership_", table[1, ]))
+      table <- table[-1, ]
+
+      dfc <- data.frame(cval)
+      colnames(dfc) <- "insider_ownership_ttm"
+      ic_table <- cbind(ticker, table, dfc)
+      colnames(ic_table)[1] <- "Symbol"
+    }
+
+    dfe <- plyr::rbind.fill(dfe, ic_table)
+    print(dfe)
   }
 
+  df <- merge_with_input(new_data = dfe, input = df, kpi = "insider_ownership")
   return(df)
 }
