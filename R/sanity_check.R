044b8b2432dae935c24a37e169a11cd6a41f4754
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2022-01-31 20:07:27 +0100

    Delete R directory

--- R/sanity_check.R
+++
@@ -1,5 +0,0 @@
-#' Wraps the sanity check regarding available ticker symbols in column 'Symbol'
-check_sanity <- function(input) {
-  message <- "Column 'Symbol' containing valid stock tickers required."
-  if (!("Symbol" %in% colnames(input))) stop(message)
-}
