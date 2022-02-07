4723034537d719fdb69e831b745b3ff3f6fc32a4
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2022-02-07 20:49:34 +0100

    Add files via upload

---
+++ R/get_price_to_tangible_book.R
@@ -0,0 +1,38 @@
+#' Get Price-Tangible-Book-Ratio
+#'
+#' @description
+#' Wrapper function for fetching data from
+#' [gurufocus.com](https://www.gurufocus.com/term/p2tangible_book/AAPL/).
+#'
+#' @usage get_price_to_tangible_book(df)
+#'
+#' @param df data.frame. Data frame with column 'symbol' containing at least one
+#' valid stock ticker symbol.
+#'
+#' @return
+#' The original data.frame supplemented by the company's
+#' **Price-Tangible-Book-Ratio** of the last five fiscal years plus current
+#' years TTM.
+#'
+#'
+#' @examples
+#' df <- data.frame('symbol' = 'AAPL')
+#' res <- get_price_to_tangible_book(df)
+#'
+#' @details
+#' The Price-Tangible-Book-Ratio measures a company's market value to its
+#' tangible assets. Tis ratio is applicable mainly to industrial or other
+#' captial-intensive companies (manufacturers, miner, ...) that own a high
+#' proportion of hard assets.
+#'
+get_price_to_tangible_book <- function(df) {
+
+  pfx <- 'tangible_book'
+  url <- 'https://www.gurufocus.com/term/p2tangible_book/'
+  xpath_tbl <- '//*[@id="target_def_historical_data"]/div[2]/div/table'
+  xpath_txt <- '//*[@id="def_body_detail_height"]/font[1]'
+
+  res <- suppressWarnings(scrape_key_fig(df, url, pfx, xpath_txt, xpath_tbl))
+
+  return(res)
+}
