e046236f629f7033a32b7e8832264fb0490ca125
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2022-02-14 19:47:55 +0100

    Add files via upload

---
+++ R/get_financial_distress.R
@@ -0,0 +1,36 @@
+#' Get Probability of Financial Distress
+#'
+#' @description
+#' Wrapper function for fetching data from
+#' [gurufocus.com](https://www.gurufocus.com/term/PFD/AAPL/).
+#'
+#' @usage get_fin_distress_prob(df)
+#'
+#' @param df data.frame. Data frame with column 'symbol' containing at least one
+#' valid stock ticker symbol.
+#'
+#' @return
+#' The original data.frame supplemented by the company's current
+#' **Probability of Financial Distress**.
+#'
+#' @examples
+#' df <- data.frame('symbol' = 'AAPL')
+#' res <- get_fin_distress_prob(df)
+#'
+#' @details
+#' The Probability of Financial Distress measures the probability that a company
+#' will go bankrupt in the upcoming year given its current financial position.
+#' The measure is obtained by a logit probability model based on eight
+#' explanatory variables.
+#'
+get_financial_distress <- function(df) {
+
+  pfx <- 'fin.distress'
+  url <- 'https://www.gurufocus.com/term/PFD/'
+  xpath_tbl <- '//*[@id="target_def_historical_data"]/div[2]/div/table'
+  xpath_txt <- '//*[@id="def_body_detail_height"]/font[1]'
+
+  res <- suppressWarnings(scrape_key_fig(df, url, pfx, xpath_txt, xpath_tbl))
+
+  return(res)
+}
