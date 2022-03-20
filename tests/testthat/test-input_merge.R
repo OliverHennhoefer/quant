4e9999538dfcb5d5bb8f6a6f3312dc2e2fc2fa9d
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2022-03-20 15:21:24 +0100

    Delete tests/testthat directory

--- tests/testthat/test-input_merge.R
+++
@@ -1,7 +0,0 @@
-test_that("Checking input_merge merging functionality", {
-  df1 <- data.frame("symbol" = "STOCK", "eps_2016" = 99)
-  df2 <- data.frame("symbol" = "STOCK", "eps_2016" = 99, "eps_2017" = 199)
-  expect_equal(
-    dim(input_merge(new_data = df1, input = df2, kpi = "eps")),
-    expected = c(1, 2))
-})
--- tests/testthat/test-scrape_key_fig.R
+++
@@ -1,9 +0,0 @@
-test_that("Checking scrape_key_fig() return data.frame", {
-  expect_equal(
-      scrape_key_fig(df = data.frame("symbol" = c("AAPL", "BABA", "PNGAY", "LMT")),
-                     url = 'https://www.gurufocus.com/term/zscore/',
-                     pfx = "zscore",
-                     xpath_txt = '//*[@id="def_body_detail_height"]/font[1]',
-                     xpath_tbl = '//*[@id="target_def_historical_data"]/div[2]/div/table')[3,3],
-    expected = 3.11)
-})
